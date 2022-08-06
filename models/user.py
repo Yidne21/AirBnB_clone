81717353c65bbd03112c5c263a612dbb257e2469
Yidnekachew Bantrga <mekuyidnedawit@gmail.com>
2022-08-06 15:06:36 +0300

    uploading user.py file

---
+++ models/user.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Defines the User class."""
+import models
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """Represents the User of the HBnB project."""
+    
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
+
+    def __init__(self, *args, **kwargs):
+        """Initialize a new User.
+        Args:
+            *args (any): Unused.
+            **kwargs (dict): Key/value pairs of attributes.
+        """
+        super().__init__(**kwargs)
